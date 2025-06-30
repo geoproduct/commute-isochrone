0bc2fcc4a4ae7200c016148cac99fb9b58acb576
geoproduct <12181187@inha.edu>
2025-07-01 01:22:52 +0900

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -12,7 +12,7 @@ app = FastAPI()
 # CORS 설정
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=["https://geoproduct.github.io"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
